ca1ff315df298329b7fe00f405c5fdb483b4ba4e
Scott Bromander <scottbromander@Scotts-MacBook-Pro.local>
2019-04-29 11:05:21 -0500

    update

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,5 +1,103 @@
 $(document).ready(onReady);
 
+let equationType = '';
+
 function onReady() {
-    console.log('Totally works');
+    $('#js-btn-equal').on('click', clickEqual);
+    $('#js-btn-add').on('click', clickAdd);
+    $('#js-btn-sub').on('click', clickSub);
+    $('#js-btn-mul').on('click', clickMul);
+    $('#js-btn-div').on('click', clickDiv);
+    $('#js-btn-C').on('click', clearInputs);
+    $('#js-btn-clear').on('click', clearHistory);
+
+    getInfo();
+}
+
+function clickAdd() {
+    equationType = 'add';
+}
+
+function clickSub() {
+    equationType = 'sub';
+}
+
+function clickMul() {
+    equationType = 'mul';
+}
+
+function clickDiv() {
+    equationType = 'div';
+}
+
+function clearInputs() {
+    $('#value1').val('');
+    $('#value2').val('');
+}
+
+function clearHistory() {
+    clearInputs();
+
+    $.ajax({
+        type: 'GET',
+        url: '/clear'
+    }).then(function (response){
+        render(response);
+    });
+}
+
+function clickEqual() {
+    const value1 = $('#value1').val();
+    const value2 = $('#value2').val();
+
+    let mathObject = {
+        value1,
+        value2,
+        equationType
+    }
+
+    console.log(mathObject);
+    postInfo(mathObject);
+}
+
+function postInfo(postObject) {
+    $.ajax({
+        type: 'POST',
+        url: '/math',
+        data: postObject
+    }).then(function(response){
+        getInfo();
+    });
+}
+
+function getInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/math'
+    }).then(function(response){
+        render(response);
+    });
+}
+
+function render(mathArray) {
+    $("#container").empty();
+    for (let mathEntry of mathArray){
+        let symbol = '';
+
+        if (mathEntry.equationType == 'add') {
+            symbol = '+';
+        } else if ((mathEntry.equationType == 'sub')) {
+            symbol = '-';
+        } else if ((mathEntry.equationType == 'mul')) {
+            symbol = '*';
+        } else if ((mathEntry.equationType == 'div')) {
+            symbol = '/';
+        }
+
+        $("#container").append(`
+            <div>
+                <p>${mathEntry.value1} ${symbol} ${mathEntry.value2} = ${mathEntry.answer}</p>
+            <div>
+        `);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const bodyParser = require('body-parser');
+
+let history = [];
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/clear', (req,res) => {
+    history = [];
+    res.send(history);
+});
+
+app.get('/math', (req,res) => {
+    res.send(history);
+});
+
+app.post('/math', (req,res) => {
+    // POST or 'add' TO THE MATH HISTORY
+    console.log(req.body);
+
+    const value1 = parseInt(req.body.value1);
+    const value2 = parseInt(req.body.value2);
+    const equationType = req.body.equationType;
+    let answer = 0;
+
+    if (equationType == 'add') {
+        answer = value1 + value2;
+    } else if(equationType == 'sub') {
+        answer = value1 - value2;
+    } else if(equationType == 'mul') {
+        answer = value1 * value2;
+    } else if(equationType == 'div') {
+        answer = value1 / value2;
+    } else {
+        console.log('Not a valid eq type!');
+    }
+
+    let historyObject = {
+        value1,
+        value2,
+        equationType,
+        answer
+    }
+
+    history.push(historyObject);
+
+    res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
